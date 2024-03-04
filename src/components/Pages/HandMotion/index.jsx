93c7117186446652131c207b5e676606cf35c05a
DONGHAEY <odonghyeon@odonghyeon-ui-MacBookAir.local>
2024-03-04 16:11:49 +0900

    카메라 띄우기

--- src/components/Pages/HandMotion/index.jsx
+++ src/components/Pages/HandMotion/index.jsx
@@ -1,4 +1,142 @@
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import { css } from "@emotion/css";
+import { Camera } from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { HAND_CONNECTIONS, Hands } from "@mediapipe/hands";
+import { drawCameraScene, drawSquare } from "./utils/drawCanvas";
+
 export const HandMotion = () => {
-  /** AI동작인식페이지(카메라, 사진찍기, 필터변경, 인스타그램 스토리 공유 or 사진 download) */
-  return null;
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const resultsRef = useRef(null);
+  const downloadRef = useRef(null);
+
+  const onResults = (results) => {
+    resultsRef.current = results;
+    const canvasCtx = canvasRef.current?.getContext("2d");
+    const width = canvasCtx.canvas.width;
+    const height = canvasCtx.canvas.height;
+    // ctx.save();
+    canvasCtx.clearRect(0, 0, width, height);
+    // ctx.scale(-1, 1);
+    // ctx.translate(-width, 0);
+
+    drawCameraScene(canvasCtx, results.image, width, height);
+
+    for (const landmarks of results?.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+        color: "#ffffff",
+        lineWidth: 4,
+      });
+      drawLandmarks(canvasCtx, landmarks, {
+        color: "#ff0000",
+        lineWidth: 10,
+        radius: 2,
+      });
+      drawSquare(canvasCtx, landmarks);
+    }
+    canvasCtx.restore();
+  };
+
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    hands.onResults(onResults);
+
+    if (webcamRef.current) {
+      const camera = new Camera(webcamRef.current.video ?? null, {
+        onFrame: async () => {
+          if (webcamRef?.current?.video) {
+            await hands.send({ image: webcamRef.current.video });
+          }
+        },
+        width: 1920,
+        height: 1080,
+      });
+      camera.start();
+    }
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          if (downloadRef.current && canvasRef.current) {
+            const href = String(canvasRef.current.toDataURL("image/jpeg"));
+            downloadRef.current.href = href;
+            downloadRef.current.download = "good.jpg";
+            downloadRef.current?.click();
+          }
+        }}
+      >
+        사진찍기
+      </button>
+      <div className={styles.container}>
+        <Webcam
+          audio={false}
+          style={{ visibility: "hidden" }}
+          width={1920}
+          height={1080}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{
+            width: 1920,
+            height: 1080,
+            facingMode: "user",
+          }}
+        />
+        <canvas
+          width={1920}
+          height={1080}
+          ref={canvasRef}
+          className={styles.canvas}
+        />
+        <a ref={downloadRef} style={{ display: "none" }} />
+      </div>
+    </>
+  );
+};
+// ==============================================
+// styles
+
+const styles = {
+  container: css`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  canvas: css`
+    position: absolute;
+    width: 1280px;
+    height: 720px;
+    background-color: #fff;
+  `,
+  buttonContainer: css`
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  `,
+  button: css`
+    color: #fff;
+    background-color: #0082cf;
+    font-size: 1rem;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 10px;
+    cursor: pointer;
+  `,
 };
