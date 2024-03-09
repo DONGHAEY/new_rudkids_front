e0504b8f6d242e53c4be9c01bf44888590aa18de
DONGHAEY <dongheaey@gmail.com>
2024-03-09 11:42:49 +0900

    a

--- src/components/Pages/List/Scene/ProductModel/index.jsx
+++ src/components/Pages/List/Scene/ProductModel/index.jsx
@@ -1,8 +1,9 @@
 import { useThree } from "@react-three/fiber";
-import { useAnimations } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import gsap from "gsap";
 import { useRef, useState } from "react";
 import { useEffect } from "react";
+import { motion } from "framer-motion-3d";
 import { ProductTag } from "./ProductTag";
 
 export const ProductModel = ({
@@ -22,6 +23,7 @@ export const ProductModel = ({
   const productModelRef = useRef(null);
 
   const { scene, animations } = gltf;
+  const portalGltf = useGLTF("/models/hand.glb");
   const { actions } = useAnimations(animations, productModelRef);
 
   const [sign, setSign] = useState(false);
@@ -49,26 +51,36 @@ export const ProductModel = ({
     }
   }, [selected, cameraX, cameraZ, three.camera.position]);
 
-  useEffect(() => {
-    if (selected) {
-      actions?.["wave"]?.reset().fadeIn(0.5).play();
-    } else {
-      actions?.["wave"]?.stop();
-    }
-  }, [selected, actions, animations]);
+  // useEffect(() => {
+  //   if (selected) {
+  //     actions?.["wave"]?.reset().fadeIn(0.5).play();
+  //   } else {
+  //     actions?.["wave"]?.stop();
+  //   }
+  // }, [selected, actions, animations]);
 
   return (
     <group
+      position-x={x}
+      position-z={z}
       onClick={() => setSelectedProductId(data.id)}
       onPointerMissed={() => setSelectedProductId(null)}
       rotation-y={Math.PI / 2 - rotation}
-      position-x={x}
-      position-z={z}
     >
       {sign && selected && (
         <ProductTag name={data.name} content={data.content} />
       )}
       <primitive ref={productModelRef} scale={2.7} object={scene} />
+      {selected && (
+        <primitive
+          scale={0.23}
+          object={portalGltf.scene}
+          rotation-y={Math.PI}
+          rotation-z={Math.PI * 0.3}
+          rotation-x={Math.PI * 0.3}
+          position={[-1, -5, 1.3]}
+        />
+      )}
     </group>
   );
 };
