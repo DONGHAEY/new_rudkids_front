697623188301bb12a87dd721ac82569017586959
DONGHAEY <dongheaey@gmail.com>
2024-04-06 23:18:58 +0900

    public dir error checking

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -17,25 +17,25 @@ const ListPage = () => {
       id: 1,
       color: "#FFE818",
       name: "My Pet Fly",
-      imageSrc: "/assets/images/List/my_pet_fly.png",
+      imageSrc: "/assets/Images/List/my_pet_fly.png",
     },
     {
       id: 2,
       color: "#FF3BA5",
       name: "A beautiful World",
-      imageSrc: "/assets/images/List/a_beautiful_world.png",
+      imageSrc: "/assets/Images/List/a_beautiful_world.png",
     },
     {
       id: 3,
       color: "#26A4FF",
       name: "!! Nothing",
-      imageSrc: "/assets/images/List/nothing.png",
+      imageSrc: "/assets/Images/List/nothing.png",
     },
     {
       id: 4,
       color: "green",
       name: "shirt",
-      imageSrc: "/assets/images/List/shirt.png",
+      imageSrc: "/assets/Images/List/shirt.png",
     },
   ].reverse();
 
