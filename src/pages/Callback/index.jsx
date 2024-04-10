821bda6a743a4a689b50df23df658c0c118aeba1
DONGHAEY <dongheaey@gmail.com>
2024-04-10 21:05:45 +0900

    cookie 가 자꾸 받아와졌는데 사라지는게 맞는지 체크중..

--- src/pages/Callback/index.jsx
+++ src/pages/Callback/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import qs from "qs";
 import { instagramLogin } from "../../apis/user/login";
+import getUser from "../../apis/user/getUser";
 
 const PlatformTypes = ["instagram"];
 
@@ -16,6 +17,7 @@ const CallbackPage = () => {
         const redirect_url = localStorage.getItem("redirect_url");
         await instagramLogin(searchParams);
         //쿠키 들어오는거 확인좀...
+        console.log(await getUser());
         // window.location.href = redirect_url;
       }
     })();
