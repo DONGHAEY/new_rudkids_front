41a319e30e42fde4c79ba03d1f791526abc42eb0
DONGHAEY <dongheaey@gmail.com>
2024-04-10 20:31:33 +0900

    login_test

--- src/apis/user/login.js
+++ src/apis/user/login.js
@@ -1,8 +1,17 @@
+import axios from "axios";
 import axiosInstance from "../axiosInstance";
 import qs from "qs";
 
 export const instagramLogin = async (searchParams) => {
-  console.log("asdfasdfasdf");
+  console.log("asdfasdfasdf", process.env.REACT_APP_SERVER_URL);
+  return await axios
+    .get(
+      `${
+        process.env.REACT_APP_SERVER_URL
+      }/auth/instagram/callback?${qs.stringify(searchParams)}`
+    )
+    .then((response) => response.data)
+    .catch((e) => e);
   return await axiosInstance
     .get(`/auth/instagram/callback?${qs.stringify(searchParams)}`)
     .then((response) => response.data)
