1f8be5d3928fe722a00b9ba8bd2a38861ef36bdc
DONGHAEY <dongheaey@gmail.com>
2024-04-10 20:36:56 +0900

    update

--- src/apis/user/getUser.js
+++ src/apis/user/getUser.js
@@ -2,6 +2,6 @@ import axiosInstance from "../axiosInstance";
 
 export const getUser = async () => {
   return await axiosInstance
-    .get("/auth/getUser")
+    .get("/api/auth/getUser")
     .then((response) => response.data);
 };
--- src/apis/user/login.js
+++ src/apis/user/login.js
@@ -4,16 +4,8 @@ import qs from "qs";
 
 export const instagramLogin = async (searchParams) => {
   console.log("asdfasdfasdf", process.env.REACT_APP_SERVER_URL);
-  return await axios
-    .get(
-      `${
-        process.env.REACT_APP_SERVER_URL
-      }/auth/instagram/callback?${qs.stringify(searchParams)}`
-    )
-    .then((response) => response.data)
-    .catch((e) => e);
   return await axiosInstance
-    .get(`/auth/instagram/callback?${qs.stringify(searchParams)}`)
+    .get(`/api/auth/instagram/callback?${qs.stringify(searchParams)}`)
     .then((response) => response.data)
     .catch((e) => e);
 };
