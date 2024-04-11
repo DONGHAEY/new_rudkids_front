97dda07309e8385855c45d60cfb7cef7208bff20
DONGHAEY <dongheaey@gmail.com>
2024-04-12 00:19:56 +0900

    update

--- src/apis/user/getUser.js
+++ src/apis/user/getUser.js
@@ -2,6 +2,8 @@ import axiosInstance from "../axiosInstance";
 
 export const getUser = async () => {
   return await axiosInstance
-    .get("/api/auth/getUser")
+    .get("/api/auth/getUser", {
+      withCredentials: true,
+    })
     .then((response) => response.data);
 };
