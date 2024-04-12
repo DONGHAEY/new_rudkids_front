f643b4485f43bfe8950de90fe68ddd93b307f116
DONGHAEY <dongheaey@gmail.com>
2024-04-12 11:20:45 +0900

    update

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  // baseURL: process.env.REACT_APP_SERVER_URL,
+  withCredentials: true,
 });
 
 export default axiosInstance;
