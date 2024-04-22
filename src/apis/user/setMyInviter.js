da229a8fabe0386e07ec64c3f0ec40596f605eea
DONGHAEY <dongheaey@gmail.com>
2024-04-22 13:44:24 +0900

    update

--- src/apis/user/setMyInviter.js
+++ src/apis/user/setMyInviter.js
@@ -2,6 +2,8 @@ import axiosInstance from "../axiosInstance";
 
 export const setMyInviter = async (inviterUserId) => {
   return await axiosInstance
-    .put(`/api/user/inviter/${inviterUserId}`)
+    .put(`/api/user/inviter/${inviterUserId}`, null, {
+      withCredentials: true,
+    })
     .then((response) => response.data);
 };
--- src/apis/user/setMySchool.js
+++ src/apis/user/setMySchool.js
@@ -2,6 +2,8 @@ import axiosInstance from "../axiosInstance";
 
 export const setMySchool = async (schoolName) => {
   return await axiosInstance
-    .put(`/api/user/school/${schoolName}`)
+    .put(`/api/user/school/${schoolName}`, null, {
+      withCredentials: true,
+    })
     .then((response) => response.data);
 };
