ec640cdfde365a0f6aee553a9813fac61ab29b72
DONGHAEY <dongheaey@gmail.com>
2024-04-26 18:34:32 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
-import { useAxiosInterceptors } from "./hooks/useAxiosInterceptors";
 import { QueryClientProvider } from "react-query";
-import queryClient from "./queries/rudkidsQueryClient";
 import useRudkidsQueryClient from "./queries/rudkidsQueryClient";
 
 function App() {
-  // useAxiosInterceptors();
-
   const [queryClient] = useRudkidsQueryClient();
 
   const routesChildren = Object.keys(routes)?.map((routeKey) => {
