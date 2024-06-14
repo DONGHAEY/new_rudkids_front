3368ad712baf9671af7d20057daf52bb88422385
DONGHAEY <dongheaey@gmail.com>
2024-06-14 21:52:45 +0900

    update cart page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -7,25 +7,17 @@ import {
   PriceWrapperUI,
   SmileSellerWrapperUI,
 } from "./styles";
-
 import CartProduct from "./CartProduct";
 import CheckoutBar from "./CheckoutBar";
 import Price from "../../shared_components/Price";
 import smileSellerSrc from "./assets/smile_kid_man.svg";
-// import eventCouponImgSrc from "./assets/coupon_1.png";
 import { useEffect, useMemo } from "react";
-import { usePopup } from "../../hooks/usePopup";
-// import PopupRoute from "../../shared_components/PopupRoute";
-// import ShippingEvent from "./ShippingEvent";
 import useCartQuery from "../../queries/cart/useCartQuery";
 import useEditShippingPriceToZeroMutation from "../../mutations/cart/useEditShippingPriceToZeroMutation";
 import { useBodyBackground } from "../../hooks/useBodyBackground";
 
 const CartPage = () => {
-  const [popupNavigate, poupPop] = usePopup();
   const { data: myCartData } = useCartQuery();
-  const editShippingPriceToZeroMutation = useEditShippingPriceToZeroMutation();
-
   const totalProductsPrice = useMemo(() => {
     if (!myCartData) return 0;
     let totalProductsPrice = 0;
@@ -38,19 +30,6 @@ const CartPage = () => {
   const totalShippingPrice = myCartData?.shippingPrice;
 
   useBodyBackground("#1a94d9");
-
-  useEffect(() => {
-    if (!myCartData) return;
-    (async () => {
-      const shippingFreeJoin = localStorage.getItem("shipping_event_join");
-      if (!shippingFreeJoin) return;
-      await editShippingPriceToZeroMutation.mutateAsync();
-      localStorage.setItem("shipping_event_joined", true);
-      alert("친구초대 수락으로, 배송비 0원을 적용했어요!");
-      localStorage.removeItem("shipping_event_join");
-    })();
-  }, [myCartData]);
-
   return (
     <PageUI>
       <Header />
@@ -69,23 +48,10 @@ const CartPage = () => {
             totalShippingPrice={totalShippingPrice}
           />
         </PriceWrapperUI>
-        {/* <div>
-          <img
-            onClick={() => {
-              popupNavigate("shipping_price_none_event");
-            }}
-            src={eventCouponImgSrc}
-            width="100%"
-          />
-        </div> */}
         <SmileSellerWrapperUI>
-          <img src={smileSellerSrc} height="200px" />
+          <img src={smileSellerSrc} width="100%" />
         </SmileSellerWrapperUI>
       </FlexWrapperUI>
-
-      {/* <PopupRoute name="shipping_price_none_event">
-        <ShippingEvent />
-      </PopupRoute> */}
       <CheckoutBar cartData={myCartData} />
     </PageUI>
   );
