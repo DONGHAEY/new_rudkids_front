6189ad1daad59eb6b5683daf80f7151669e04bde
DONGHAEY <dongheaey@gmail.com>
2024-06-18 22:44:11 +0900

    update :: SymbolLoader

--- src/shared_components/SymbolLoader/index.jsx
+++ src/shared_components/SymbolLoader/index.jsx
@@ -2,44 +2,61 @@ import { useEffect, useRef, useState } from "react";
 import { LoaderUI } from "./styles";
 import gsap from "gsap";
 import PublicBizAssets from "../../global/public-biz-assets";
+import { atom, useRecoilState } from "recoil";
 
-const SymbolLoader = ({ loading, position = "fixed", minDelaySec = 1 }) => {
+const lastDurationAtom = atom({
+  key: "symbol_loader_last_duration",
+  default: 0,
+});
+
+let tl = null;
+const SymbolLoader = ({ loading, position = "fixed" }) => {
+  const [lastD, setLastD] = useRecoilState(lastDurationAtom);
   const loaderRef = useRef();
-  const [timeS, setTimeS] = useState(0);
+  const symbolRef = useRef();
   const [show, setShow] = useState(true);
 
   useEffect(() => {
-    gsap.set(loaderRef.current, {
-      opacity: 1,
-    });
-  }, []);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setTimeS(timeS + 1);
-    }, 1000);
-    return () => {
-      clearInterval(timeout);
-    };
-  }, [timeS]);
-
-  useEffect(() => {
-    if (timeS >= minDelaySec && !loading) {
-      gsap.to(loaderRef.current, {
-        opacity: 0,
-        duration: 1,
+    tl = gsap.timeline();
+    tl.to(symbolRef.current, {
+      scale: 1,
+      duration: 0.5,
+    })
+      .to(symbolRef.current, {
+        scale: 1.5,
+        duration: 0.5,
+        ease: "back.in",
+      })
+      .to(symbolRef.current, {
+        scale: 1,
+        duration: 0.5,
+        yoyo: true,
         onComplete: () => {
-          setShow(false);
+          if (!loading) {
+            gsap.to(loaderRef.current, {
+              opacity: 0,
+              duration: 1,
+              delay: 0.5,
+              onComplete: () => {
+                setShow(false);
+              },
+            });
+          } else {
+            tl.startTime(0);
+          }
         },
       });
-    }
-  }, [timeS, loading]);
+    tl.startTime(lastD);
+    return () => {
+      setLastD(tl?.duration() ?? 0);
+    };
+  }, [loading]);
 
   if (!show) return null;
 
   return (
     <LoaderUI ref={loaderRef} position={position}>
-      <img src={PublicBizAssets.symbol} width="70%" />
+      <img ref={symbolRef} src={PublicBizAssets.symbol} width="70%" />
     </LoaderUI>
   );
 };
