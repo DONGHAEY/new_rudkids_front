fcc53a6279a010d58f9f0cb7b63a55aae643a4c6
DONGHAEY <dongheaey@gmail.com>
2024-07-02 22:31:06 +0900

    test update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Suspense, useEffect } from "react";
 import { QueryClientProvider } from "react-query";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import GlobalStyle from "../src/styles";
 import { routes } from "./routes";
 import useRudkidsQueryClient from "./rudkidsQueryClient";
@@ -10,31 +10,35 @@ import Loader from "./shared_components/Loader";
 
 function App() {
   const [queryClient] = useRudkidsQueryClient();
+  const { pathname } = useLocation();
 
   useWindowScrollInit();
 
+  // const currentRoute = Object.values(routes)?.find(
+  //   (route) => route.path === pathname
+  // );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        {Object.values(routes)?.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={
-              <Suspense fallback={route.fallback ?? <Loader />}>
-                {route.viewTrack ? (
-                  <DefaultTrackPageView
-                    pageName={route.name}
-                    children={<route.element routeInfo={route} />}
-                  />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {Object.values(routes)?.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                route.viewTrack ? (
+                  <DefaultTrackPageView pageName={route.name}>
+                    <route.element routeInfo={route} />
+                  </DefaultTrackPageView>
                 ) : (
                   <route.element routeInfo={route} />
-                )}
-              </Suspense>
-            }
-          />
-        ))}
-      </Routes>
+                )
+              }
+            />
+          ))}
+        </Routes>
+      </Suspense>
       <GlobalStyle />
     </QueryClientProvider>
   );
